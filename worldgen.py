c98b1ae7779ba445f4d17888bc7a662393a8b18e
Elvin Yung <elvinyung1@gmail.com>
2014-07-25 04:51:35 -0400

    added check to reject unsatisfactory worlds

--- worldgen.py
+++ worldgen.py
@@ -4,7 +4,7 @@ import sys, random, math
 
 # HELPER FUNCTIONS
 
-debug_mode = False
+debug_mode = True
 
 # float_equal check if two floats are equal
 def float_equal(a,b):
@@ -30,7 +30,7 @@ def white_noise(w,h, weight_points=[], proximity=5.0):
 						grid[y][x] += math.min(random.random(), 1.0-grid[y][x])
 
 
-	if debug_mode: print grid
+	#if debug_mode: print grid
 	return grid
 
 # THE ACTUAL ALGORITHM
@@ -113,21 +113,18 @@ def generate_noise(w, h, octaves=[4,5,6,7,8]):
 
 # converts a 2d grid into their corresponding terrain type
 # TODO: make the terrain dict dynamic to the data given
-def threshold_noise_to_terrain(grid):
+def apply_thresholds(grid):
 
 	# analyze grid to determine terrain
 
 	# determine the data mean and std.dev, then use that to set the range
-	#  tentative: < -1mu = sea, -1mu to 1mu = land, > 1mu = mountain 
 
 	grid_width = len(grid[0])
 	grid_height = len(grid)
 
 	avg = sum([sum(row) for row in grid])/(grid_width*grid_height)
-	print avg
 	variance = sum([(tile-avg)**2 for tile in row for row in grid])/(grid_width*grid_height)
 	stdev = variance**0.5
-	print stdev
 
 	terrain = [(avg-(0.6*stdev), ' '), (avg+(1.25*stdev), '.'), (1.1, '^')]
 
@@ -146,6 +143,37 @@ def threshold_noise_to_terrain(grid):
 		ret.append(ret_row)
 	return ret
 
+
+# generate world: keeps generating worlds until a satisfactory one is found
+def generate_world(w, h, terrain_composition, acceptable_margin=0.05):
+	# terrain_composition is a list of tile to percentage tuples, eg. [(' ',0.3),('.',0.5),('^',0.2)]
+
+	octaves = range(int(math.log(min(w,h),2)))[-5:] # takes the last 5 powers of 2 closest to min(w,h)
+	if debug_mode: print 'octaves: ' + str(octaves)
+
+	world_area = w*h
+	worlds_generated = 0
+
+	satisfactory_world = False # True if the world has the correct percentages of terrain
+	while not satisfactory_world:
+		world = apply_thresholds(generate_noise(w, h, octaves))
+		if debug_mode: print 'world #' + str(worlds_generated) + '\n' + prettify_grid(world)
+		worlds_generated += 1
+		if debug_mode: print 'generated world #' + str(worlds_generated)
+		satisfactory_world = True
+
+		# checks for every tile if the actual composition is within an acceptable margin
+		for (tile,proportion) in terrain_composition:
+			tile_proportion = sum([row.count(tile) for row in world])/float(world_area)
+			if debug_mode: print 'proportion of \'' + tile + '\' in world #' + str(worlds_generated) + ': ' + str(tile_proportion)
+			if abs(tile_proportion - proportion) > acceptable_margin:
+				satisfactory_world = False
+				break
+
+		if debug_mode and satisfactory_world: print 'world #' + str(worlds_generated) + ' passed!'
+
+	return world
+
 # prints a 2d array of characters without spaces between
 def prettify_grid(grid):
 	return '\n'.join([''.join(row) for row in grid])
@@ -158,9 +186,8 @@ List of options:
 
 if __name__ == "__main__":
 	try:
-		debug_mode = '--debug' in sys.argv
-		noise_grid = generate_noise(int(sys.argv[1]),int(sys.argv[2]))
-		print prettify_grid(threshold_noise_to_terrain(noise_grid))
+		debug_mode = ('--debug' in sys.argv)
+		print prettify_grid(generate_world(int(sys.argv[1]),int(sys.argv[2]), [(' ',0.4),('.',0.5),('^',0.1)]))
 
 	except:
 		usage()
--- worldgen_tests.py
+++ worldgen_tests.py
@@ -9,18 +9,19 @@ class AvgFnTestCase1(unittest.TestCase):
 		print str(grid1).replace(']','\n').replace('[[','').replace(', [','')
 		grid2 = worldgen.scale_grid(worldgen.white_noise(5,5),2)
 		print str(grid2).replace(']','\n').replace('[[','').replace(', [','')
+
 		print str(worldgen.avg_grids([grid2, grid1])).replace(']',',').replace('[[','').replace(', [','')
+
+		# not really a test case, it just prints out the data so I can use it to run external tests
 		assert(True)
 
-'''
 class WorldGenTestCase1(unittest.TestCase):
 	def runTest(self):
-		noise_grids = worldgen.generate_noise(128,128)
-		print noise_grids
-		print ''
-		print worldgen.prettify_grid(worldgen.threshold_noise_to_terrain(noise_grids))
+		worldgen.generate_world(64, 64, [(' ',0.3),('.',0.5),('^',0.2)])
+
+		# assert that the map has 
+
 		assert(True)
-'''
 
 if __name__ == '__main__':
 	unittest.main()
